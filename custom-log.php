774783bf1e6417b906327331329af2f5e89e645b
alex vidal <avidal@greenbull.group>
2020-01-02 11:18:36 +0100

    add log function

--- custom-log.php
+++ custom-log.php
@@ -9,3 +9,13 @@
 * Copyright (c) 2019 KVDC
 * Description : Write to a custom log
 */
+function custom_logs($message) {
+    if(is_array($message)) {
+        $message = json_encode($message);
+    }
+    $file = fopen(WPMU_PLUGIN_DIR . "/custom_logs.log","a");
+    echo fwrite($file, "\n" . date('Y-m-d h:i:s') . " :: " . $message);
+    fclose($file);
+} ;
+
+//custom_logs("Hey! I am in a header");
