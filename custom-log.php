4d7b4263587b3e96717d0b261ba3890363ce66f5
alex vidal <avidal@greenbull.group>
2020-01-02 11:08:01 +0100

    add file header

--- custom-log.php
+++ custom-log.php
@@ -1,1 +1,11 @@
-
+<?php
+/*
+* Created Date: Thursday, September 12th 2019, 2:30:43 pm
+* Author: Alex Vidal
+* -----
+* Last Modified: Thu Jan 01 2020
+* Modified By: Alex Vidal
+* -----
+* Copyright (c) 2019 KVDC
+* Description : Write to a custom log
+*/
